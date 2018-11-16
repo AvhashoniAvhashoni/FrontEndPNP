65503213c0695a55be82ebdc774cb180f661a13e
Avhashoni Netshiozwi <avhashoninetshiozwi@gmail.com>
2018-11-16 10:33:52 +0200

    commited on 2018/11/16 10:34

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterHeaderComponent } from './register-header/register-header.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterSmartShopperComponent } from './register-smart-shopper/register-smart-shopper.component';
+import { RegisterOnlineComponent } from './register-online/register-online.component';
+import { RegisterSmartShopperRegComponent } from './register-smart-shopper-reg/register-smart-shopper-reg.component';
+import { ItemComponent } from './item/item.component';
+import { LoginComponent } from './login/login.component';
+import { LoginHeaderComponent } from './login-header/login-header.component';
+import { AisleComponent } from './aisle/aisle.component';
+import { ProductComponent } from './product/product.component';
+import { ItemsComponent } from './items/items.component';
+
+const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:'app-home'},
+  {path:'app-root', component:AppComponent },
+  {path:'app-header', component:HeaderComponent },
+  {path: 'app-home', component:HomeComponent },
+  {path: 'app-footer', component:FooterComponent },
+  {path: 'app-register-header', component:RegisterHeaderComponent },
+  {path: 'app-register', component:RegisterComponent },
+  {path: 'app-register-smart-shopper', component:RegisterSmartShopperComponent },
+  {path: 'app-register-online', component:RegisterOnlineComponent },
+  {path: 'app-register-smart-shopper-reg', component:RegisterSmartShopperRegComponent },
+  {path: 'app-item', component:ItemComponent },
+  {path: 'app-login', component:LoginComponent },
+  {path: 'app-login-header', component:LoginHeaderComponent },
+  {path: 'app-item', component:ItemComponent },
+  {path: 'app-aisle', component:AisleComponent },
+  {path: 'app-product', component:ProductComponent },
+  {path: 'app-items', component:ItemsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { PicknpayService } from './picknpay.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ItemComponent } from './item/item.component';
+import { LoginComponent } from './login/login.component';
+import { LoginHeaderComponent } from './login-header/login-header.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterHeaderComponent } from './register-header/register-header.component';
+import { RegisterOnlineComponent } from './register-online/register-online.component';
+import { RegisterSmartShopperRegComponent } from './register-smart-shopper-reg/register-smart-shopper-reg.component';
+import { RegisterSmartShopperComponent } from './register-smart-shopper/register-smart-shopper.component';
+import { AisleComponent } from './aisle/aisle.component';
+import { ProductComponent } from './product/product.component';
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    ItemComponent,
+    LoginComponent,
+    LoginHeaderComponent,
+    RegisterComponent,
+    RegisterHeaderComponent,
+    RegisterOnlineComponent,
+    RegisterSmartShopperRegComponent,
+    RegisterSmartShopperComponent,
+    AisleComponent,
+    ProductComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [PicknpayService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
