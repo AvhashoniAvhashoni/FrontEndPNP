8e4e69265189783bb02679dc2b87a6bf25916d92
Avhashoni Netshiozwi <avhashoninetshiozwi@gmail.com>
2018-11-21 15:41:32 +0200

    2018/11/21 03:42

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  
+  select = "select all";
+  collaps = "collepse all";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  slct() {
+    if (this.select == "select all") {
+      this.select = "deselect all";
+      this.select.fontcolor('003359');
+    } else {
+      this.select="select all";
+    }
+  }
+
+  clp() {
+    if (this.collaps == "collepse all") {
+      this.collaps = "expend all";
+      this.collaps.fontcolor('B9154A');
+    } else {
+      this.collaps = "collepse all";
+    }
   }
 
 }
